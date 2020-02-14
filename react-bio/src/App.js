1447d6e79fa8bb0166abc03168e7e4e78400cc51
unknown <12siricoa94@gmail.com>
2020-02-14 17:51:16 -0500

    turned navbar into a component and put into components folder

--- react-bio/src/App.js
+++ react-bio/src/App.js
@@ -1,20 +1,13 @@
 import React, {Component} from 'react';
 import './css/index.css';
+import Navbar from './components/navbar';
 
 
 class App extends Component {
   render (){
     return (
       <div className="App">
-        <nav>
-          <h2 class="logo">Warcraft 3 Guide</h2>
-          <ul class="nav-menu">
-            <li><a class="nav-link" href="">Home</a></li>
-            <li><a class="nav-link" href="">Races</a></li>
-            <li><a class="nav-link" href="">Heroes</a></li>
-            <li><a class="nav-link" href="">Strategy</a></li>
-          </ul>
-        </nav>
+        <Navbar></Navbar>
       </div>
     );
   }
---
+++ react-bio/src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => (
+    <nav>
+        <h2 class="logo">
+            <a class="logo-link" href="">Warcraft 3 Guide</a>
+        </h2>
+        <ul class="nav-menu">
+            <li><a class="nav-link" href="">Home</a></li>
+            <li><a class="nav-link" href="">Races</a></li>
+            <li><a class="nav-link" href="">Heroes</a></li>
+            <li><a class="nav-link" href="">Strategy</a></li>
+        </ul>
+    </nav>
+);
+
+export default Navbar;
