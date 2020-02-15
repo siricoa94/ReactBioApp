5d43020b08eabaf97719521c779ff0b1dc4c54aa
unknown <12siricoa94@gmail.com>
2020-02-14 20:24:08 -0500

    Created homepage with images and thumbnails

--- react-bio/src/App.js
+++ react-bio/src/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import './css/index.css';
 import Navbar from './components/navbar';
+import Home from './components/home';
 
 
 class App extends Component {
   render (){
     return (
       <div className="App">
-        <Navbar></Navbar>
+        <Navbar title="Mirana's Guide to Warcraft 3"></Navbar>
+        <Home title="Warcraft 3 Guide"></Home>
       </div>
     );
   }
--- react-bio/src/components/navbar.js
+++ react-bio/src/components/navbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Navbar = () => (
+const Navbar = (props) => (
     <nav>
-        <h2 class="logo">
-            <a class="logo-link" href="">Warcraft 3 Guide</a>
+        <h2 className="logo">
+            <a className="logo-link" href="">{props.title}</a>
         </h2>
         <ul class="nav-menu">
-            <li><a class="nav-link" href="">Home</a></li>
-            <li><a class="nav-link" href="">Races</a></li>
-            <li><a class="nav-link" href="">Heroes</a></li>
-            <li><a class="nav-link" href="">Strategy</a></li>
+            <li><a className="nav-link" href="">Home</a></li>
+            <li><a className="nav-link" href="">Races</a></li>
+            <li><a className="nav-link" href="">Heroes</a></li>
+            <li><a className="nav-link" href="">Strategy</a></li>
         </ul>
     </nav>
 );
